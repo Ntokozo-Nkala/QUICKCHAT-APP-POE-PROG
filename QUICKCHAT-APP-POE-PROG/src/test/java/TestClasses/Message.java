420f3f23e5c0cca8639e7c10b0e950d74a77faa7
Mbali Ntokozo Nkala <166023505+Ntokozo-Nkala@users.noreply.github.com>
2026-06-13 00:05:54 +0200

    Update Message.java

--- QUICKCHAT-APP-POE-PROG/src/test/java/TestClasses/Message.java
+++ QUICKCHAT-APP-POE-PROG/src/test/java/TestClasses/Message.java
@@ -1,5 +1,5 @@
 package TestClasses;
-import com.mycompany.quickchat.app.poe.prog.Message;
+import com.mycompany.quickchat.app.poe.prog;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
